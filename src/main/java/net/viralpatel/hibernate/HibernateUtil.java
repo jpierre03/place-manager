4b8ac85d6a8d1ee6d9f2d0c9314d6fe446ad6733
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2012-06-25 20:59:59 +0200

    empty lines

--- src/main/java/net/viralpatel/hibernate/HibernateUtil.java
+++ src/main/java/net/viralpatel/hibernate/HibernateUtil.java
@@ -1,12 +1,12 @@
 package net.viralpatel.hibernate;
- 
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
- 
+
 public class HibernateUtil {
- 
+
     private static final SessionFactory sessionFactory = buildSessionFactory();
- 
+
     private static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
@@ -18,7 +18,7 @@ public class HibernateUtil {
             throw new ExceptionInInitializerError(ex);
         }
     }
- 
+
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
