ebf2d251eae4c18ce851a3477e678d370abfab1e
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2012-06-25 20:56:51 +0200

    disable actions & indentation

--- src/main/java/net/viralpatel/hibernate/Main.java
+++ src/main/java/net/viralpatel/hibernate/Main.java
@@ -1,110 +1,124 @@
 package net.viralpatel.hibernate;
 
-import java.sql.Date;
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
+import java.sql.Date;
+import java.util.List;
 
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		
-		// Read
-		System.out.println("******* READ *******");
-		List employees = list();
-		System.out.println("Total Employees: " + employees.size());
-		
-		
-		// Write
-		System.out.println("******* WRITE *******");
-		Employee empl = new Employee("Jack", "Bauer", new Date(System.currentTimeMillis()), "911");
-		empl = save(empl);
-		empl = read(empl.getId());
-		System.out.printf("%d %s %s \n", empl.getId(), empl.getFirstname(), empl.getLastname());
-		
-		
-		
-		// Update
-		System.out.println("******* UPDATE *******");
-		Employee empl2 = read(1l); // read employee with id 1
-		System.out.println("Name Before Update:" + empl2.getFirstname());
-		empl2.setFirstname("James");
-		update(empl2);	// save the updated employee details
-		
-		empl2 = read(1l); // read again employee with id 1
-		System.out.println("Name Aftere Update:" + empl2.getFirstname());
-		
-		
-		// Delete
-		System.out.println("******* DELETE *******");
-		delete(empl); 
-		Employee empl3 = read(empl.getId());
-		System.out.println("Object:" + empl3);
-	}
-	
-	
+    public static void main(String[] args) {
 
-	private static List list() {
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = sf.openSession();
 
-		List employees = session.createQuery("from Employee").list();
-		session.close();
-		return employees;
-	}
-	private static Employee read(Long id) {
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = sf.openSession();
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
 
-		Employee employee = (Employee) session.get(Employee.class, id);
-		session.close();
-		return employee;
-	}
-	private static Employee save(Employee employee) {
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = sf.openSession();
+        session.beginTransaction();
 
-		session.beginTransaction();
+        Long id = (Long) session.save(new Employee("Jack","Bauer",new Date(444), "911"));
+        //employee.setId(id);
 
-		Long id = (Long) session.save(employee);
-		employee.setId(id);
-		
-		session.getTransaction().commit();
-		
-		session.close();
+        session.getTransaction().commit();
 
-		return employee;
-	}
+        session.close();
+//        // Write
+//        System.out.println("******* WRITE *******");
+//        Employee empl = new Employee("Jack", "Bauer", new Date(System.currentTimeMillis()), "911");
+//        empl = save(empl);
+//        empl = read(empl.getId());
+//        System.out.printf("%d %s %s \n", empl.getId(), empl.getFirstname(), empl.getLastname());
+//
+//        // Read
+//        System.out.println("******* READ *******");
+//        List employees = list();
+//        System.out.println("Total Employees: " + employees.size());
+//
+//        // Update
+//        System.out.println("******* UPDATE *******");
+//        Employee empl2 = read(1l); // read employee with id 1
+//        System.out.println("Name Before Update:" + empl2.getFirstname());
+//        empl2.setFirstname("James");
+//        update(empl2);    // save the updated employee details
+//
+//        empl2 = read(1l); // read again employee with id 1
+//        System.out.println("Name Aftere Update:" + empl2.getFirstname());
+//
+//        // Read
+//        System.out.println("******* READ *******");
+//        List employees2 = list();
+//        System.out.println("Total Employees: " + employees2.size());
+//
+//
+//        // Delete
+//        System.out.println("******* DELETE *******");
+//        delete(empl);
+//        Employee empl3 = read(empl.getId());
+//        System.out.println("Object:" + empl3);
+    }
 
-	private static Employee update(Employee employee) {
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = sf.openSession();
 
-		session.beginTransaction();
+    private static List list() {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
 
-		session.merge(employee);
-		
-		session.getTransaction().commit();
-		
-		session.close();
-		return employee;
+        List employees = session.createQuery("from Employee").list();
+        session.close();
+        return employees;
+    }
 
-	}
+    private static Employee read(Long id) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
 
-	private static void delete(Employee employee) {
-		SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = sf.openSession();
-		
-		session.beginTransaction();
-		
-		session.delete(employee);
-		
-		session.getTransaction().commit();
-		
-		session.close();
-	}
-	
+        Employee employee = (Employee) session.get(Employee.class, id);
+        session.close();
+        return employee;
+    }
+
+    private static Employee save(Employee employee) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+        session.beginTransaction();
+
+        Long id = (Long) session.save(employee);
+        //employee.setId(id);
+
+        session.getTransaction().commit();
+
+        session.close();
+
+        return employee;
+    }
+
+    private static Employee update(Employee employee) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+        session.beginTransaction();
+
+        session.merge(employee);
+
+        session.getTransaction().commit();
+
+        session.close();
+        return employee;
+
+    }
+
+    private static void delete(Employee employee) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+        session.beginTransaction();
+
+        session.delete(employee);
+
+        session.getTransaction().commit();
+
+        session.close();
+    }
+
 }
